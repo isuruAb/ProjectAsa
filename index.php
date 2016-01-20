48f546b1c36375d33a59efab8c9174d66959c51a
vpisabeywardana <vpisabeywardana@gmail.com>
2016-01-21 02:17:34 +0530

    new file register.php

--- index.php
+++ index.php
@@ -1,10 +1,28 @@
 <html>
     <head>
+      <!--Import Google Icon Font-->
+      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <link rel="stylesheet" type="text/css" href="style.css" />
     </head>
+
     <body>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+    
         <div class="container">
             
+            <nav class="blue darken-4">
+              <ul class="right hide-on-med-and-down">
+                <li><a href="#!">New for Aza</a></li>
+                <li><a href="#!">Login</a></li>
+              </ul>
+            </nav>
+
         </div>
     </body>
 </html>
